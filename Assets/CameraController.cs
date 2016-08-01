e6208148e0e8b87ee25dfac9fe3ac166ee374a9f
Arun Thomas <arunthms01@gmail.com>
2016-08-01 13:22:37 +0530

    step 1 : scrolling background added

---
+++ Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+
+    public Transform target;
+    public float smoothing;
+
+    Vector3 offset;
+    float LowY;
+	// Use this for initialization
+	void Start () {
+
+
+        offset = transform.position - target.position;
+        LowY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Vector3 targetCamPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing*Time.deltaTime);
+
+        if (transform.position.y < LowY) transform.position = new Vector3(transform.position.x, LowY, transform.position.z);
+	}
+}
