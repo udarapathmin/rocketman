78e0ca4727298dbcb9ed25d21da88ebc51f3694f
Arun Thomas <arunthms01@gmail.com>
2016-08-02 23:03:18 +0530

    Man flyes in the rotTED DIRECTION

--- Assets/Scripts/RotationController.cs
+++ Assets/Scripts/RotationController.cs
@@ -4,14 +4,15 @@ using System.Collections;
 public class RotationController : MonoBehaviour {
     
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public Transform target;
 
+    void Awake() {
+       
+    }
 
     void Update()
     {
+      
         if (Input.GetMouseButton(0))
         {
             Debug.Log("Pressed left click.");
@@ -28,6 +29,22 @@ public class RotationController : MonoBehaviour {
                 transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             }
 
+        } if (Input.GetMouseButtonUp(0)) {
+
+           
+            Debug.Log("Released left click.");
+            Vector3 finalAngle = transform.localEulerAngles;
+            Rigidbody2D playerBody = GetComponent<Rigidbody2D>();
+            playerBody.isKinematic = false;
+            //playerBody.AddForce(new Vector2(1, 0) * 0.5f, ForceMode2D.Impulse);
+            //target.GetComponent<Rigidbody2D>().AddForceAtPosition(new Vector2(finalAngle.z, finalAngle.z), new Vector2(transform.position.x, transform.position.y), ForceMode2D.Impulse);
+            //Rigidbody2D playerBody = GetComponent<Rigidbody2D>();
+            //playerBody.isKinematic = false;
+
+            playerBody.AddForceAtPosition(new Vector2(finalAngle.z, finalAngle.z), new Vector2(transform.position.x, transform.position.y), ForceMode2D.Impulse);
+            
         }
     }
 }
+
+
