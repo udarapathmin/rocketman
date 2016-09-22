ac1655e7b6f988843d0c5ad05e36ea32c6ef9ad8
Udara Karunarathna <udarapathmin@gmail.com>
2016-09-22 19:12:08 +0530

    Other menu buttons added

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour {
+
+	public void playGame()
+    {
+        SceneManager.LoadScene("Start_scean");
+    }
+}
